e796474f4788414e4cd1f42ceb075f047001003f
Ramond Falcão <ramondfalcao@gmail.com>
2022-09-08 16:23:09 -0300

    Feat: Adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes - Desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+  {
+    nome: { $nin: ["McChicken"] },
+  },
+  { 
+    $addToSet: { ingredientes: "ketchup" },
+  },
+);
+
+db.produtos.find({}, { _id: false, nome: true, ingredientes: true });
