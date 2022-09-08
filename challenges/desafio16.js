f3bec3756e61b35c06fbe30a806530859056f668
Ramond Falcão <ramondfalcao@gmail.com>
2022-09-08 17:19:54 -0300

    Feat: Adicione o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac - Desafio 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,15 @@
+db.produtos.updateOne(
+  {
+    nome: "Big Mac",
+  },
+  { 
+   $currentDate: { ultimaModificacao: true },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: { $exists: true },
+  },
+  { _id: false, nome: true },
+);
